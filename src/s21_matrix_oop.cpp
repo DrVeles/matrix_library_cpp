6d2cb6ec0957164bb72cab3cb81002cf3835e8b6
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-05-10 18:58:10 +0300

    quick save

--- src/s21_matrix_oop.cpp
+++ src/s21_matrix_oop.cpp
@@ -1,8 +1,1 @@
 #include "s21_matrix_oop.h"
-
-int main(){
-
-    std::cout << "HELLO niggas" << std::endl;
-
-    return 0;
-}
---
+++ src/s21_matrix_oop_tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "s21_matrix_oop.h"
+
+int main() {
+  std::cout << "HELLO world" << std::endl;
+
+  int* ptr = new int;
+  delete ptr;
+
+  return 0;
+}
